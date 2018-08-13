b41d5eabf3456ae23484bc42aa78a5209a5d1905
sourav-prasad <39465749+sourav-prasad@users.noreply.github.com>
2018-08-14 00:22:59 +0530

    Update RegressionModel.py

--- RegressionModel.py
+++ RegressionModel.py
@@ -15,7 +15,7 @@ y = dataset.iloc[:, :1].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 12)
-X_train, X_val, y_train, y_val   = train_test_split(X_train, y_train, test_size=0.2, random_state=12)
+
 
 
 # Fitting Multiple Linear Regression to the Training set
