4c33578edb399f7239f95b503e3f744d7cf34e5b
sourav-prasad <39465749+sourav-prasad@users.noreply.github.com>
2018-08-31 14:06:58 +0530

    Update LinReg.py

--- LinReg.py
+++ LinReg.py
@@ -19,8 +19,8 @@ y = dataset.iloc[:, :1].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-#X_train = sc.fit_transform(X_train)
-#X_test = sc.transform(X_test)
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 lm = LinearRegression()
 lm.fit(X_train, y_train)
 predicted = lm.predict(X_test)
